a3333537c57ba7d1d1f1ec3a33535c271342df34
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 10:22:14 +0000

    Checking data-type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/** Main: Checking various sizes of data type
+ *
+ *Return sizes after compilation
+ */
+int main(void)
+{
+char a;
+int i;
+long l;
+long long int lli;
+float f;
+printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long: %lu byte(s)\n" , (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+return(0);
+}
