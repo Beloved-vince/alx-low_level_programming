a02a4f668f45bd1b639e1c43c46f8dee5a2d331f
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 09:14:29 +0000

    simple C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main entry point
+ *main - entry point
  *
- * return: 0 after printing function
+ *Return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
