7d3396104b694373e373b18015a32b05c5dca10a
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 11:47:49 +0000

    Commit simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-write("ind that piece of art is useful\"  - Dora Korpar, 2015-10-19\n");
+write("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
