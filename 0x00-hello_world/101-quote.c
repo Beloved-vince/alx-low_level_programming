40eff51f04c8e1134e90101404b1280b8add1f1d
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 11:53:57 +0000

    Commit update simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<unistd.h>
 /**
- *main - Entry point
- * Return: Always 0 (Success)
+ * main - prints the standrad error sandwiched around the  standard outpu/t
  *
+ * Return: returns 1
  */
 int main(void)
 {
-write("and that piece of art is useful\"  - Dora Korpar, 2015-10-19\n");
-
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
