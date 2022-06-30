c76938f4724fa87a3c56624ad2fade176c82b847
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 09:00:41 +0000

    simple C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@ iinclude <stdio.h>
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
