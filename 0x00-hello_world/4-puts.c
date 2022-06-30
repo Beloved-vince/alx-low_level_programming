816b89e720b994cc9edbda8f09f392e7904a6b8c
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 08:29:47 +0000

    commit this message as a starter

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main entry point
+ * return 0 after printing function
+ */
+int main (void)
+{
+	puts("Programming is like building a multilngual puzzle \n");
+	return(0);
+}
---
+++ 0x00-hello_world/tutoriial.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+int i;
+double d;
+char c;
+	printf("Thesize of an int i: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a double i : %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of c is : %lu.\n", (unsigned long)sizeof(c));
+
+	return (0);
+
+}
