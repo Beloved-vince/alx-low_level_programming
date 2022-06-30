10f9a6d51aacfe0191df883ffb58937ee1e1aaa0
Beloved-vince <vinceoludare@gmail.com>
2022-06-30 10:49:20 +0000

    Data-type update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int i;
 long int li;
 long long int lli;
 float f;
-printf("Size of a char: %lu byte(s) \n",(unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s)\n" ,(unsigned long)sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(lli));
-printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-return(0);
+return (0);
 }
