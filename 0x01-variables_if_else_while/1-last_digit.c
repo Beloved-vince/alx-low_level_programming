c743ac74494e6af3ba5b8bfe7d16aa862466a693
Beloved-vince <vinceoludare@gmail.com>
2022-07-01 23:28:16 +0000

    little bit technical for me with betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	printf("Last digit of %d is %i and is greater than 5\n", n, n % 10);
 	}
 
-	if (n == 0)
+	if (n % 10 == 0)
 	{
 	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
