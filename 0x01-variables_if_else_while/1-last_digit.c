c414d0ec73220ef46d7f4e354545fd244daddd86
Beloved-vince <vinceoludare@gmail.com>
2022-07-01 23:22:03 +0000

    little bit technical for me with betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,17 +16,17 @@ int main(void)
 	/* your code goes there */
 	if ((n % 10) > 5)
 	{
-	printf("Last digit of %d is %i and is greater than 5", n, n % 10);
+	printf("Last digit of %d is %i and is greater than 5\n", n, n % 10);
 	}
 
 	if (n == 0)
 	{
-	printf("Last digit of %d is 0", n);
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 
 	if ((n % 10) < 6 && (n % 10) != 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 
 	}
 	return (0);
