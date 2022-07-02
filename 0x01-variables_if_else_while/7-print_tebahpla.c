4babed821e73c5c5d4233a3a8f50181033c46cbd
Beloved-vince <vinceoludare@gmail.com>
2022-07-02 16:06:43 +0000

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always o (success)
+ *
+ */
+int main(void)
+{
+	char reverse_alpha ='z';
+	while (reverse_alpha >= 'a')
+	{
+		putchar(reverse_alpha);
+		reverse_alpha--;
+	}
+		putchar('\n');
+	return (0);
+}
