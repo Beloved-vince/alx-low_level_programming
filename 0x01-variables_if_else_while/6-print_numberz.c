bd207a898f2db3851927930324171367b67f583f
Beloved-vince <vinceoludare@gmail.com>
2022-07-02 15:51:28 +0000

    base 10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int base_10;
+	
+	for (base_10 = 0; base_10 < 10; base_10++)
+	{
+		putchar(base_10);
+	}
+	putchar('\n');
+	return (0);
+}
