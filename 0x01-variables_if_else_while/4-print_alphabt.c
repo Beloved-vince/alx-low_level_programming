0d56475bd5c28405bd5cc21418375be7e86f7c4f
Beloved-vince <vinceoludare@gmail.com>
2022-07-02 14:51:37 +0000

    Except e q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main (void)
+{
+	char lower_case = 'a';
+	
+	while(lower_case <= 'z')
+	{
+	
+		if (lower_case != 'e' && lower_case != 'q')
+		{
+			putchar(lower_case);
+		}
+
+			lower_case++;
+	}
+	putchar('\n');
+	return (0);
+}
