c88547b08f6a50d69cf7ea84dabef790e246beb0
Beloved-vince <vinceoludare@gmail.com>
2022-07-07 17:47:33 +0000

    please commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -18,3 +18,24 @@ c++;
 _putchar('\n');
 
 }
+#include "main.h"
+
+/**
+ * print_alphabet - Begin entry
+ *Return: 0
+*/
+
+void print_alphabet(void)
+{
+char c = 'a';
+
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+
+}
+_putchar('\n');
+
+}
+
