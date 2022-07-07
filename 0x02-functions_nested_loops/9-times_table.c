f1af753c943ae4b6fa8fc89443ab3436fe668606
Beloved-vince <vinceoludare@gmail.com>
2022-07-07 20:28:18 +0000

    Time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * times_table -  a function that prints the 9 times table, starting with 0
  * rone = row, cone = column, d = digits of current result
