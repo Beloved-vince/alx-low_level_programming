d7842dfc2684c5dd994d12cc3afc5ee6853e089a
Beloved-vince <vinceoludare@gmail.com>
2022-07-08 21:39:29 +0000

    commit message

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if parameter is a digit between 0 and 9
+ * @c: input number
+ * Return: 1 if digit between 0 and 9, 0 if otherwise
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
