b8f00167f8010961387276dde6968f6da4102632
Beloved-vince <vinceoludare@gmail.com>
2022-07-12 22:48:08 +0000

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _strlen - find the lenght of a string
+ * and return it in integer form
+ *@s: String variable
+ *
+ * Return: nothing but in mainfile
+ */
+int _strlen(char *s)
+{
+	*s = NULL;
+
+	int len = strlen(*s);
+
+	printf(len);
+
+}
