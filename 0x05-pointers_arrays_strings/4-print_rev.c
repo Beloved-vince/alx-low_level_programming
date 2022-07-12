f0204aa0866758e2bd47c8d5a685aa6275ffd613
Beloved-vince <vinceoludare@gmail.com>
2022-07-12 23:49:30 +0000

    reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - reverse order
+ * printing in reverse order
+ * @s: Function string
+ * Return: 0
+ */
+void print_rev(char*s)
+{
+	puts(strrev(s));
+}
