606bc187cc7716fcd7bb0a7327aef4443df8fd86
Beloved-vince <vinceoludare@gmail.com>
2022-07-12 20:47:31 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/** main- Entry Levl 
+ *@n: Return Update
+ *
+ * Return: 0 Success
+ */
+void reset_to_98(int *n)
+{
+	int p;
+
+	*n = 98;
+
+	_putchar(reset_to_98);
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+#endif
