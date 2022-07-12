fd6b2d06236f03bcb269f8be6ca21400e46487ff
Beloved-vince <vinceoludare@gmail.com>
2022-07-12 23:13:01 +0000

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * int _strlen - find the lenght of a string
  * and return it in integer form
- *@s: String variable
+ * @s: String variable
  *
  * Return: nothing but in mainfile
  */
@@ -12,11 +11,10 @@ int _strlen(char *s)
 {
 	int count;
 
-	for (count = 0; count < '\0'; count++)
+	while (s[count] != '\0')
 	{
-		s[count];	
+		count++	
 	}
 
-	printf("%d\n", count);
-
+	return (count);
 }
