fca86c57f41458a033cf9e1d77cafc96da7c797b
Beloved-vince <vinceoludare@gmail.com>
2022-07-12 20:54:54 +0000

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "main.h"
-/** main- Entry Levl 
- *@n: Return Update
- *
- * Return: 0 Success
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ * and updates the value it points to 98
+ * @n: int parameter
+ * Return: Nothing
  */
 void reset_to_98(int *n)
 {
-	int p;
 
 	*n = 98;
-
-	return (0);
 }
