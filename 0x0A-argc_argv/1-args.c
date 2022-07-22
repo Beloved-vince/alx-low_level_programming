fbb1b6614a5e60d2e460cb9fdb67311e87f3c455
Beloved-vince <vinceoludare@gmail.com>
2022-07-22 22:55:33 +0000

    program that prints all arguments it receives.

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - entry point to print the number of string
- * @argc: Size of array to be printed
- * @argv: String variable
- * Return: Return the number of string passed as arguement
- */
+ * main - program that prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: array with the arguments
+ *
+ * Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
-		(void)argv;
+	(void)argv;
 
-		printf("%d \n", argc - 1);
-
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
