1ef369eb5d833ece8b6fba68e5989be0084831f9
Beloved-vince <vinceoludare@gmail.com>
2022-07-22 13:33:09 +0000

    function that returns the factorial of a given number. Using recursion boss

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - factorial of a given number decreasing by 1
+ * @n: variable decreased by one
+ * Return: n*(n-1)
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * (n - 1));
+}
