fcb7b94ab1ae64af0f420f087e6dd21da311fb5f
Beloved-vince <vinceoludare@gmail.com>
2022-07-22 21:48:40 +0000

    prime number with recursion without for loop

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * is_prime_number - checking for prime number
+ * @n: variable to hold random num
+ * Return: return 1 if prime and 0 if not
+ */
+int is_prime_number(int n)
+{
+	int a = 2;
+
+	if (n == 1)
+		return (0);
+	if (n < 0)
+		return (0);
+	return (calculo_primo(n, a));
+}
+int calculo_primo(int num_, int inf);
+/**
+ * calculo_primo - function that returns 1 if the input integer is a prime.
+ *@num_: first value
+ *@inf: second value
+ *
+ * Return: Return solution
+ */
+int calculo_primo(int num_, int inf)
+{
+	if (num_ / 2 < inf)
+		return (1);
+	if (num_ % inf == 0)
+		return (0);
+	return (calculo_primo(num_, inf + 1));
+}
