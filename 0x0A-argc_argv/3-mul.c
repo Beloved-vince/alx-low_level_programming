aedbf6377e404650ed671bb4053fc610b7d63f46
Beloved-vince <vinceoludare@gmail.com>
2022-07-23 00:03:56 +0000

    A program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Size of input data
+ * @argv: data to be inserted
+ * Return: multiplication of argv(two data) integer
+ */
+int main(int argc, char *argv[])
+{
+	int to_int;
+	int count;
+
+	char c[] = "Error";
+
+	if (argc != 3)
+	{
+		printf("%s\n", c);
+		return (1);
+	}
+
+	for (count = 1; count < argc; count++)
+
+	{
+		to_int = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d \n", to_int);
+	}
+		return (0);
+}
