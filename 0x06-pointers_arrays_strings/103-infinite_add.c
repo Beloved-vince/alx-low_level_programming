8ad1c24611062b168225eab5087a83d08173887c
Beloved-vince <vinceoludare@gmail.com>
2022-07-24 19:17:25 +0000

    a function that adds two numbers.

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ *
+ * @b: char pointer
+ *
+ * @size: int
+ *
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+
+	int i, j;
+
+	i = 0;
+
+	while (i < size)
+	{
+		printf("%08x: ", i);
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+				printf("%02x", (unsigned char)b[j]);
+			else
+				printf("  ");
+			if (j % 2)
+				putchar(' ');
+		}
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+				break;
+
+			if (b[j] >= ' ' && *(b + j) <= '~')
+				putchar(*(b + j));
+			else
+				putchar('.');
+		}
+		i += 10;
+		if (i < size)
+			putchar('\n');
+	}
+	putchar('\n');
+}
