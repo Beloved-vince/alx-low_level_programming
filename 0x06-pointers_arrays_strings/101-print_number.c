7abf381b1dc04671d6bd9ee24de84954640a91f5
Beloved-vince <vinceoludare@gmail.com>
2022-07-24 19:26:40 +0000

    print numbers only

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,34 +9,5 @@
 
 void print_number(int n)
 {
-
-	int len, index;
-
-	len = 1000000000;
-
-	if (n < 0)
-	{
-		_putchar('-');
-
-	if (n == -2147483648)
-	{
-		index = 1;
-		n += 1;
-	}
-	n = -n;
-	}
-	while (len != 1)
-	{
-		if (n >= len)
-			_putchar((n / len) % 10 + '0');
-		len /= 10;
-	}
-	if (index == 1)
-	{
-		_putchar(((n % 10) + 1) + '0');
-	}
-	else
-	{
-		_putchar(n % 10 + '0');
-	}
+	_putchar(n);
 }
