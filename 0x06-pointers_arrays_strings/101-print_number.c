6ab9d6fd5c1fbe29765316ad79e9d93f935049d0
Beloved-vince <vinceoludare@gmail.com>
2022-07-24 19:24:21 +0000

    print numbers only

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,42 @@
-#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_number -Prints an integer
+ *
+ * @n: input integer
+ *
+ * Return: void
+ */
 
-int main(void)
+void print_number(int n)
 {
-  int n;
-  int a[5];
-  int *p;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  p[5] = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	int len, index;
+
+	len = 1000000000;
+
+	if (n < 0)
+	{
+		_putchar('-');
+
+	if (n == -2147483648)
+	{
+		index = 1;
+		n += 1;
+	}
+	n = -n;
+	}
+	while (len != 1)
+	{
+		if (n >= len)
+			_putchar((n / len) % 10 + '0');
+		len /= 10;
+	}
+	if (index == 1)
+	{
+		_putchar(((n % 10) + 1) + '0');
+	}
+	else
+	{
+		_putchar(n % 10 + '0');
+	}
 }
