3192cb93541bf307fdf51f209fba41ce6bf215a0
Beloved-vince <vinceoludare@gmail.com>
2022-07-26 22:45:28 +0000

    string duplicate with memory allocation

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup - copy of the string given as a parameter.
+ *
+ * @str: unsigned int
+ *
+ * Return: char pointer
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int counter1, counter2;
+	char *strCopy;
+
+	counter1 = 0;
+	counter2 = 0;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	while (str[counter1] != '\0')
+	{
+		counter1++;
+	}
+
+	strCopy = malloc(sizeof(char) * (counter1 + 1));
+
+	if (strCopy == NULL)
+	{
+		return (NULL);
+	}
+
+	while (counter2 < counter1)
+	{
+		strCopy[counter2] = str[counter2];
+		counter2++;
+	}
+
+	strCopy[counter2] = '\0';
+
+	return (strCopy);
+}
