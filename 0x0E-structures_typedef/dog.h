7480235e49049ab75a201e682421af5bee111bfd
Beloved-vince <vinceoludare@gmail.com>
2022-08-01 21:27:27 +0000

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,28 +1,22 @@
-#ifndef HEADER
-#define HEADER
+#ifndef DOG
+#define DOG
 /**
- * struct dog - Dog attributes
- * @name: The Dog name
- * @age: The Dog age
- * @owner: The Dog owner's name
+ * struct dog - define a new structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description: The Dog attribute
+ * Description: Strcture with info about a dog
  */
 struct dog
 {
-
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for the Dog structure
- */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
