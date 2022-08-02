4249c7e89f0a4ce8b9c6871c80c3d64840cf6918
beloved-vince <vinceoludare@gmail.c>
2022-08-02 19:22:15 +0000

    structure

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function
+ * @d: free all element of dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+			free(d->owner);
+	}
+}
