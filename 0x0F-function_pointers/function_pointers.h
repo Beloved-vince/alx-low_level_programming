ddb1255726f857be3749d02da63a212ee98c0f7a
beloved-vince <vinceoludare@gmail.c>
2022-08-03 17:53:10 +0000

    commit function

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
-#ifndef FUNCTION
-#define FUNCTION
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stdio.h>
 int _putchar(char c);
-
-#endif
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif /* _FUNCTION_POINTERS */
