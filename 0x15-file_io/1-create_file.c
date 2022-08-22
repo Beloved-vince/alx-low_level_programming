b8de2d6aea899bf851c3143de0d5dd78307c58b6
beloved-vince <belovedvince@gmail.com>
2022-08-22 20:39:51 +0000

    creating a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = " ";
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	len = write(fd, text_content, strlen(text_content));
+
+	if ((fd == -1) | (len == -1))
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
