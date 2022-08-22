238e3b14c15764c40d649eb12c522452c5677a62
beloved-vince <belovedvince@gmail.com>
2022-08-22 15:56:22 +0000

    File reading

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * read_textfile - A prototype for Read a file
+ * @filename: name of file to read to stdout
+ * @letters: number of letters in the file
+ * Return: file and number of letters in it
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, sz, count, str;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (buf == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+	{
+		return (0);
+		exit(1);
+	}
+	count = read(fd, buf, letters);
+	buf[count] = '\0';
+
+	close(fd);
+
+	sz = open(filename, O_WRONLY);
+
+	if (sz < 0)
+	{
+		return (0);
+		exit(1);
+	}
+
+	str = write(STDOUT_FILENO, buf, letters);
+
+	if (str < 0)
+	{
+		return (0);
+	}
+
+	return (str);
+}
