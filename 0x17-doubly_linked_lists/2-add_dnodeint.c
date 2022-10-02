dddfe121fd73d2afe9b7cd43cb89c429bb816890
Beloved-vince <vinceoludare@gmail.com>
2022-10-02 14:49:33 +0000

    adding node to the first

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,14 +9,12 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t* new_node = malloc(sizeof(dlistint_t));
-
 	new_node->n = n;
 	new_node->next = (*head);
 	new_node->prev = NULL;
-/**
+
 	if ((*head) != NULL)
 		(*head)->prev = new_node;
-*/	
 	(*head) = new_node;
-return new_node;
+	return new_node;
 }
