a7e0872f3a83a38418b5dcd2960c7361f755fcad
Beloved-vince <vinceoludare@gmail.com>
2022-10-02 15:43:45 +0000

    free DLL

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
-
+/**
+ * free_dlistint - frees a listint_t list
+ * @head: head of linked list
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *free_node;
