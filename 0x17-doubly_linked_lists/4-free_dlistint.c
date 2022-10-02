b17054e278a0b099b2420c4480933bf391fa02e5
Beloved-vince <vinceoludare@gmail.com>
2022-10-02 15:50:02 +0000

    free DLL

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -6,16 +6,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *free_node;
 
 	if (head == NULL)
 		return;
-	free_node = head;
 
 	while (head)
 	{
-		free_node = head;
 		head = head->next;
-		free(free_node);
+		free(head);
 	}
 }
